8e33053acb89d35e1fad4f8d7450da87b991056d
git <20200122>
2020-04-13 11:19:00 +0800

    data-list123

---
+++ 0413/a20200413-data-list2.php
@@ -0,0 +1,88 @@
+<?php
+require __DIR__ . '/__connect_db.php';
+$page_name = 'data-list2';
+
+?>
+<?php include __DIR__ . '/parts/html-head.php'; ?>
+<?php include __DIR__ . '/parts/navbar.php'; ?>
+    <style>
+        tbody tr i.fa-trash-alt {
+            color: red;
+        }
+    </style>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <!--
+                        <li class="page-item ">
+                            <a class="page-link" href="?page=">
+                                <i class="fas fa-arrow-circle-left"></i>
+                            </a>
+                        </li>
+                        <li class="page-item ">
+                            <a class="page-link" href="?page="></a>
+                        </li>
+                        <li class="page-item ">
+                            <a class="page-link" href="?page=">
+                                <i class="fas fa-arrow-circle-right"></i>
+                            </a>
+                        </li>
+                        -->
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Mobile</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Birthday</th>
+                        <th scope="col">Address</th>
+                    </tr>
+                    </thead>
+                    <tbody class="data-tbody">
+                    <!--
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+<?php include __DIR__ . '/parts/scripts.php'; ?>
+    <script>
+        /*
+            運作的流程 steps
+            1. 取得資料 (包成 function
+            2. 生頁碼列的按鈕
+            3. 生資料表格
+         */
+        const pagination = $('.pagination'),
+            tbody = $('.data-tbody')
+
+        function getDataByPage(page=1){
+            $.get('data-list2-api.php', {page:page}, function(data){
+                console.log(data);
+            }, 'json');
+        }
+
+        getDataByPage();
+
+    </script>
+<?php include __DIR__ . '/parts/html-foot.php'; ?>
